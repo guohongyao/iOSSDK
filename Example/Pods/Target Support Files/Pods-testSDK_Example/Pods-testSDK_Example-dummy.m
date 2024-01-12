26d376936619d7ecaf74b7d5868764fef38358ef
GuoHongyao <guohongyao@noxgroup.com>
2024-01-12 17:09:59 +0800

    修改.podspace文件

---
+++ Example/Pods/Target Support Files/Pods-testSDK_Example/Pods-testSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_testSDK_Example
+@end
---
+++ Example/Pods/Target Support Files/testSDK/testSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_testSDK : NSObject
+@end
+@implementation PodsDummy_testSDK
+@end
